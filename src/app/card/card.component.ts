28366f676007430054fc4d47efb7ce37d83cbd4f
hershellayton95 <filidm5795@gmail.com>
2023-03-10 10:29:27 +0100

    lifecycle

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
+  constructor() {
+    console.log("0 costruttore");
+  }
+
+
+
+
+  ngOnChanges(): void {
+    console.log("on changes --- questa non dovrebbe funzionare");
+  }
+  ngOnInit(): void {
+    console.log("1 Init");
+  }
+  ngDoCheck(): void {
+    console.log("2 e 6 e 8 do check");
+  }
+  ngAfterContentInit(): void {
+    console.log("3 after content init");
+  }
+  ngAfterContentChecked(): void {
+    console.log("4 e 7 e 9 content checked");
+  }
+  ngAfterViewInit(): void {
+    console.log("5 after view init");
+  }
+  ngAfterViewChecked(): void {
+    console.log("5 after view checked");
+  }
+  ngOnDestroy(): void {
+    console.log("on destroy");
+  }
 }
