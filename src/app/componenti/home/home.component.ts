b8ad8ddd4612bfa20e672e3bb40a8422be2c32ad
hershellayton95 <filidm5795@gmail.com>
2023-03-15 09:55:42 +0100

    observe

--- src/app/componenti/home/home.component.ts
+++ src/app/componenti/home/home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  sottoscrizione!: any;
 
+  ngOnInit(): void {
+    this.sottoscrizione = interval(1000).subscribe((observer) => {
+      console.log(observer);
+    });
+
+    // creiamo noi
+    // new Observable((observer) => {
+    //   let count = 0;
+    //   setInterval(() => {
+    //     observer.next(count);
+    //     count++;
+    //   }, 1000);
+    // }).subscribe((number) => {
+    //   console.log(number);
+    // });
+  }
+  ngOnDestroy(): void {
+    this.sottoscrizione.unsubscribe();
+  }
 }
